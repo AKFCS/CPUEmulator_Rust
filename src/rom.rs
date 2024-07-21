58bd965a66a2ca42e8688184b0d911666f1c8e91
erslie <kbyk0416@gmail.com>
2024-07-21 11:50:36 +0900

    rom func

--- src/rom.rs
+++ src/rom.rs
@@ -2,4 +2,16 @@ pub struct Rom {
     pub memory_array: Vec<u8>,
 }
 
-impl Rom 
+impl Rom {
+    pub fn new(memory_array: Vec<u8>) -> Self{
+        Self { memory_array }
+    }
+
+    pub fn read(&self, pc: u8) -> u8 {
+        self.memory_array[pc as usize]
+    }
+
+    pub fn size(&self) -> u8 {
+        self.memory_array.len() as u8
+    }
+}
