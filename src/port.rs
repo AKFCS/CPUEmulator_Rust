7f0cd18a480a187d39f402a47e22e37a6ec99d82
erslie <kbyk0416@gmail.com>
2024-07-21 16:07:08 +0900

    add port

---
+++ src/port.rs
@@ -0,0 +1,22 @@
+pub struct Port {
+    input: u8,
+    output: u8,
+}
+
+impl Port {
+    pub fn new(input: u8, output: u8) -> Self {
+        Self { input,output }
+    }
+
+    pub fn input(&self) -> u8 {
+        self.inupt
+    }
+
+    pub fn output(&self) -> u8 {
+        self.output
+    }
+
+    pub fn set_output(&mut self, im: u8) {
+        self.output = im;
+    } 
+}
