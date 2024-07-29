8734eeb3e4f3f0043d3db68b9f9d15aec1854809
erslie <kbyk0416@gmail.com>
2024-07-29 19:59:01 +0900

    chore

--- src/emulator.rs
+++ src/emulator.rs
@@ -17,9 +17,9 @@ impl CpuEmulator {
             rom.size() <= 16,
         );
         Self {
-            register,
-            port,
-            rom,
+            register: Register,
+            port: Port,
+            rom: Rom,
         }
     }
 
@@ -43,6 +43,7 @@ impl CpuEmulator {
                 |Opcode::MovA
                 |Opcode::MovB
                 |Opcode::MovA2B
+                |Opcode::MovB2A
                 |Opcode::Jmp
                 |Opcode::Jnc
                 |Opcode::OutIm => Ok(opecode, im),
@@ -60,21 +61,30 @@ impl CpuEmulator {
             let(opecode, im) = self.decode(data)?;
 
             match  opecode {
-                Opcode::MovA => self.mov_a(im),
-                Opcode::MovB => self.mov_b(im),
-                Opcode::AddA => self.add_a(im),
-                Opcode::AddB => self.add_b(im),
-                Opcode::MovA2B => self.mov_a2b(),
-                Opcode::MovB2A => self.mov_b2a(),
-                Opcode::Jmp => self.jmp();
-                Opcode::
-                Opecode::
-                Opecode::
-                Opecode::
-                Opecode::
+                Opecode::MovA => self.mov_a(im),
+                Opecode::MovB => self.mov_b(im),
+                Opecode::AddA => self.add_a(im),
+                Opecode::AddB => self.add_b(im),
+                Opecode::MovA2B => self.mov_a2b(),
+                Opecode::MovB2A => self.mov_b2a(),
+                Opecode::Jmp => self.jmp(im),
+                Opecode::Jnc => self.jnc(im),
+                Opecode::OutIm => self.out_im(im),
+                Opecode::InA => self.in_a(),
+                Opecode::InB => self.in_b(),
+                Opecode::OutB => self.out_im(im),
+            };
+
+            if opecode != Opecode::Jmp && opecode != Opecode::Jnc {
+                self.register.incr_pc();
+            }
+
+            if self.does_halt() {
+                return Ok(());
             }
         }
     }
+
 
     fn does_halt(&self) -> bool {
         self.register.pc() >= self.rom.size()
@@ -92,17 +102,17 @@ impl CpuEmulator {
 
     fn mov_a2b(&mut self) {
         let register_b = self.register.register_b();
-        self.register.set_register_a(new_value:register_b);
-        self.register.carry_flag(new_value:0);
+        self.register.set_register_a(register_b);
+        self.register.carry_flag(0);
     }
 
     fn mov_b2a(&mut self) {
         let register_a = self.register.register_a();
-        self.register.set_register_b(new_value: register_a);
-        self.register.set_carry_flag(new_value:0);
+        self.register.set_register_b(register_a);
+        self.register.set_carry_flag(0);
     }
 
-    fn add_a(&mut self, im; u8) {
+    fn add_a(&mut self, im: u8) {
         let existence = self.register.register_a();
         let new_value = existence + im;
 
